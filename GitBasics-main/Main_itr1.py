b43924c5fee0fe3749c890a98d564cc927002b9a
pavlova1221 <holmberggg12@gmail.com>
2025-07-19 12:27:01 +0400

    добавила валидацию модели

--- GitBasics-main/Main_itr1.py
+++ GitBasics-main/Main_itr1.py
@@ -3,24 +3,19 @@ from aiogram import Bot, Dispatcher
 from Handlers import Dispatch, Help, Survey
 from Handlers.Encoder import decoded_bot_key, test_key
 from multiprocessing import freeze_support
-#>> git@github.com:Faso-main/DA_Bot.git
+from LM.Res import results
 
 logging.basicConfig(
     level=logging.INFO,
     format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
-    handlers=[logging.FileHandler("bot.log"), logging.StreamHandler()]) # одновременно в файл и консоль
+    handlers=[logging.FileHandler("bot.log"), logging.StreamHandler()]
+)
 
 logger = logging.getLogger()
-
-Developer_mode = False  # (True - показ времени и точности в ответе, False - вывод только ответа)
-Train_new_model = False  # (True - тренировка модели, False - использование готовой)
-Load_from_huggingface_model = True # Установите True для загрузки из Hugging Face (игнорируется, если Train_new_model = True)
-SERVER_TIME = 0  # переменная работы с часовым поясом (4 по гринвичу при запуске на VPS)
 
 token = os.getenv("TELEGRAM_BOT_TOKEN", test_key)
 bot = Bot(token)
 dp = Dispatcher()
-
 
 async def main() -> None:
     try:
@@ -29,12 +24,17 @@ async def main() -> None:
         dp.include_router(Help.router_Help)
         await bot.delete_webhook(drop_pending_updates=True)
         await dp.start_polling(bot)
-    except Exception as e: logging.error(f"Error in main: {e}")
-    finally: await bot.close()
+        print(f"Results of validation: {results}")
+    except Exception as e:
+        logging.error(f"Error in main: {e}")
+    finally:
+        await bot.close()
 
 if __name__ == '__main__':
-    print(f'Digital assistant is running.............')
-    try: asyncio.run(main())
-    except KeyboardInterrupt: logging.info("Bot stopped by user.")
-    except Exception as e: logging.error(f"Error: {e}")
-
+    print('Digital assistant is running.............')
+    try:
+        asyncio.run(main())
+    except KeyboardInterrupt:
+        logging.info("Bot stopped by user.")
+    except Exception as e:
+        logging.error(f"Error: {e}")
